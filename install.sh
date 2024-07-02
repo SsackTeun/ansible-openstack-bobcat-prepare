10c76fd8798b8f9b991f09e106f6f88e1a2da613
jaeyoung645@naver.com <jaeyoung645@naver.com>
2024-07-02 04:03:16 +0000

    install.sh 참고

---
+++ install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+python3 -m venv venv/
+. venv/bin/activate
+echo ". venv/bin/activate" > activate
+
+pip install -r requirements.txt
+
+# pip 업그레이드
+pip install -U pip
+
+# ansible 설치
+#pip install 'ansible-core>=2.14,<2.16'
+pip install 'ansible-core>=2.14,<2.16'
+
+# yum 패키지 설치
+dnf install -y gcc git python3-libselinux libffi-devel openssl-devel openssl net-tools traceroute vim unzip iputils sshpass
+
+# ansible collection
+ansible-galaxy collection install containers.podman --force
+
+git clone https://github.com/tomasr/molokai.git ./molokai
+mkdir ~/.vim/colors
+cp ./molokai/colors/molokai.vim ~/.vim/colors/
+echo  "colorscheme molokai
+let g:molokai_original = 1
+let g:rehash256 = 1
+" >> ~/.vimrc
+rm -rf molokai/
---
+++ ssh-copy-id.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# KEY-GEN
+dnf install sshpass -y
+ssh-keygen -t rsa -b 4096 -f "~/.ssh/id_rsa" -N ""
+
+PASSWORD="P@ssw0rd"
+
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.21
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.22
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.23
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.31
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.32
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.33
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.11
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.12
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.13
+sshpass -p $PASSWORD ssh-copy-id root@10.130.39.41
+
