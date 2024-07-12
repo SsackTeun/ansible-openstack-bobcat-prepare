bd71a131be5a2f10d04181925caf66efc4d79524
jaeyoung <jaeyoung645@naver.com>
2024-07-12 07:35:49 +0000

    02_test 테스트 중

--- ssh-copy-id.sh
+++ ssh-copy-id.sh
@@ -6,15 +6,19 @@ ssh-keygen -t rsa -b 4096 -f "/root/.ssh/id_rsa" -N ""
 
 PASSWORD="P@ssw0rd"
 
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.21
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.22
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.23
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.31
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.32
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.33
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.11
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.12
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.13
-sshpass -p $PASSWORD ssh-copy-id root@10.130.39.41
-#sshpass -p $PASSWORD ssh-copy-id -p 10975 root@192.168.24.7
+servers=(
+    "10.130.39.21"
+    "10.130.39.22"
+    "10.130.39.23"
+    "10.130.39.31"
+    "10.130.39.32"
+    "10.130.39.11"
+    "10.130.39.12"
+    "10.130.39.13"
+    "10.130.39.42"
+)
 
+for server in "${servers[@]}"
+do
+    sshpass -p "$PASSWORD" ssh-copy-id -f "root@$server"
+done
